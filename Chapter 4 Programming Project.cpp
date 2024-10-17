b4f17d64ed42cf5baffb88fe80a2d290fe9876e9
jjw86297 <jjw86297@email.vccs.edu>
2024-10-16 21:45:24 -0400

    First Commit

--- Chapter 4 Programming Project.cpp
+++ Chapter 4 Programming Project.cpp
@@ -1,20 +1,46 @@
-// Chapter 4 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Jacob Walrath
+//10/16/2024
+//Chapter 4 Programming Project
+//Shipping_Charges
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    double weight, distance;
+    double rate = 0.0;
+    double total = 0.0;
+
+    cout << "Enter the weight of the package in kilograms (maximum of 20 kg): ";
+    cin >> weight;
+
+    cout << "Enter the distance that the package will be shipped\n";
+    cout << "(minimum of 10 miles and maximum of 3,000 miles): ";
+    cin >> distance;
+
+    if ((weight <= 2) && (weight > 0))
+        rate = 1.10;
+    else if (weight > 2 && weight <= 6)
+        rate = 2.20;
+    else if (weight > 6 && weight <= 10)
+        rate = 3.70;
+    else if (weight > 10 && weight <= 20)
+        rate = 4.80;
+    else if ((weight <= 0) || (weight > 20))
+        cout << "You did not enter a valid weight.\n";
+
+    if ((distance < 10) || (distance > 3000))
+        cout << "You did not enter a valid distance.\n";
+
+    total = (distance / 500) * rate;
+
+    if (rate != 0.0)
+    {
+        if ((distance >= 10) && (distance <= 3000))
+            cout << "The total for charges is " << total << ".";
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
