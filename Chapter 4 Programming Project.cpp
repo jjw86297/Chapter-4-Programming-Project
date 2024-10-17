353083303d5d4b8182c991f5115064fd942050ec
jjw86297 <jjw86297@email.vccs.edu>
2024-10-16 20:28:17 -0400

    First Commit

--- Chapter 4 Programming Project.cpp
+++ Chapter 4 Programming Project.cpp
@@ -1,20 +1,55 @@
-// Chapter 4 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Jacob Walrath
+//10/16/2024
+//Chapter 4 Programming Project
+//Days_in_A_Month
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int days, leap_year = 0; 
+    int month, year;
+
+    cout << "Enter a month (1-12): ";
+    cin >> month;
+
+    cout << "Enter a year: ";
+    cin >> year;
+
+    if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+        leap_year = 1;
+    // Tests if the year is divisible by certain numbers so it can be calculated if it is a leap year or not (specifically for February).
+        
+    if (month == 2)
+    {
+        if (leap_year == 1)
+        {
+            days = 29;
+            cout << days << " days";
+        }
+        else
+        {
+            days = 28;
+            cout << days << " days";
+        }
+    }
+
+    else if ((month % 2 != 0) && (month <= 12))
+    {
+        days = 30;
+        cout << days << " days";
+    }
+
+    else if ((month % 2 == 0) && (month <= 12) && (month != 2))
+    {
+        days = 31;
+        cout << days << " days";
+    }
+
+    else
+        cout << "You didn't enter a valid number for the month.";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
